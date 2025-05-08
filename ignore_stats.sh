4ebcf5e66e6af2d8ec45bb285297e05c66054031
Aaron <blastanders@users.noreply.github.com>
2025-05-08 10:29:02 +1000

    Create ignore_stats.sh

---
+++ ignore_stats.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Find all public_html/stats directories under /home
+for statsdir in /home/*/public_html/stats; do
+  if [ -d "$statsdir" ]; then
+    echo "Processing $statsdir"
+
+    # Create .gitignore file with the specified content
+    cat > "$statsdir/.gitignore" << 'EOF'
+##### ignore this dir #####
+*
+**
+**/*
+##### ignore this dir #####
+EOF
+
+    echo "Created .gitignore in $statsdir"
+
+    # Move to the git root (assumes statsdir is inside a Git repo)
+    git_root=$(git -C "$statsdir" rev-parse --show-toplevel 2>/dev/null)
+
+    if [ -n "$git_root" ]; then
+      rel_path=${statsdir#$git_root/}  # Get relative path to the git root
+      echo "Removing $rel_path from Git cache"
+      git -C "$git_root" rm --cached -r "$rel_path"
+    else
+      echo "Not inside a Git repository: $statsdir"
+    fi
+  fi
+done
+
+echo "Done processing all stats directories"
